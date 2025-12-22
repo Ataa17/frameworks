41a48058c2b2c737494dc1665b4b86f173aeef2c
Ataa17 <ataa.mars@yahoo.com>
2025-12-22 14:44:37 +0100

    routing and file structure done

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './layouts/auth-layout/auth-layout';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthLayout,
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./features/auth/login/login').then(m => m.Login)
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('./features/auth/register/register').then(m => m.Register)
+      },
+      { path: '', redirectTo: 'login', pathMatch: 'full' }
+    ]
+  },
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./features/dashboard/dashboard/dashboard').then(m => m.Dashboard)
+      },
+      {
+        path: 'create-post',
+        loadComponent: () => import('./features/create-post/create-post/create-post').then(m => m.CreatePost)
+      },
+      {
+        path: 'analytics',
+        loadComponent: () => import('./features/analytics/analytics/analytics').then(m => m.Analytics)
+      },
+      {
+        path: 'settings',
+        loadComponent: () => import('./features/settings/settings/settings').then(m => m.Settings)
+      },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+    ]
+  }
+];
